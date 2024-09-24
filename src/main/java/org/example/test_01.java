b839e07aa8818dc20cac106bfffce4fdc2aa8362
sprol <sprolo81@gmail.com>
2024-09-23 21:28:14 -0300

    tercer world

---
+++ src/main/java/org/example/test_01.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class test_01 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world world");
+        System.out.println("Welcome to the tercer world");
+    }
+}
