eff4d91a815554ad5dbe84521f94e6a81efff48f
sprol <sprolo81@gmail.com>
2024-09-24 22:15:11 -0300

    agrego apellido en prueba de concepto

--- src/main/java/org/example/test_01.java
+++ src/main/java/org/example/test_01.java
@@ -4,6 +4,6 @@ public class test_01 {
     public static void main(String[] args) {
 
         System.out.println("Hello world world");
-        System.out.println("Welcome to the tercer world");
+        System.out.println("HI SEBASTIAN PROLO");
     }
 }
